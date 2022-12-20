cd0b7729951fa2448d82d9fcef017ebf34c956cf
kevin <kevinli@oitc.com.tw>
2022-12-20 16:20:10 +0800

    加入專案檔案。

---
+++ multi-sample/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace multi_sample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.Name = "textBox1";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            resources.ApplyResources(this.contextMenuStrip1, "contextMenuStrip1");
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Name = "label1";
+
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ multi-sample/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Resources;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace multi_sample
+{
+
+    public partial class Form1 : Form
+    {
+        private static CultureInfo CI;
+        private static ResourceManager rm;
+        private static string userUICulture;
+
+        private void InitUICulture()
+        {
+            //userUICulture = Thread.CurrentThread.CurrentUICulture.Name;
+            //switch (userUICulture)
+            //{
+            //    case "en":
+            //        CI = new CultureInfo("en");
+            //        Thread.CurrentThread.CurrentUICulture = CI;
+            //        break;
+            //    case "zh-TW":
+            //        CI = new CultureInfo("zh-TW");
+            //        Thread.CurrentThread.CurrentUICulture = CI;
+            //        //MessageBox.Show("zh-TW");
+            //        break;
+            //    case "zh-CN":
+            //        CI = new CultureInfo("zh-Hans");
+            //        Thread.CurrentThread.CurrentUICulture = CI;
+            //        break;
+
+            //    default:
+            //        userUICulture = Thread.CurrentThread.CurrentUICulture.Name;
+            //        break;
+            //}
+            rm = new ResourceManager("multi_sample.Properties.dict", Assembly.GetExecutingAssembly());
+
+        }
+
+        private static void langconv(ref string msg)
+        {
+            string check_msg="";
+
+            try
+            {
+                check_msg = rm.GetString(msg);
+                if (check_msg != null)
+                {
+                    msg = check_msg;
+                }
+            }
+            catch (Exception ex)
+            {
+                //---- Enter your exception handling code here -----
+                throw ex;
+            }
+
+        }
+
+
+
+        public Form1()
+        {
+            //Thread.CurrentThread.CurrentUICulture = new CultureInfo("en");
+            System.Globalization.CultureInfo currentUICulture = System.Threading.Thread.CurrentThread.CurrentUICulture;
+            InitializeComponent();
+            InitUICulture();
+        }
+
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string test_msg = "Hello World";
+            langconv(ref test_msg);
+            MessageBox.Show(test_msg);
+            MessageBox.Show(rm.GetString("test_data"));
+        }
+    }
+}
